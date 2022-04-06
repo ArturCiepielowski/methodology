6c90e2f84b22cde0a5fa63c5f241276e6f28b065
Jakub Pilarek <pilarek.jakub@gmail.com>
2022-04-06 19:59:27 +0200

    zrobione

--- src/main/java/com/sda/arppl2/Main.java
+++ src/main/java/com/sda/arppl2/Main.java
@@ -3,5 +3,12 @@ package com.sda.arppl2;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Zadanie 1 wykonane");
+        Osoba osoba = new Osoba("Marian","Kowalski");
+        Osoba innaOsoba = new Osoba("Adrian","Roztocki");
+        Osoba innaOsoba1 = new Osoba("Alicja", "Wkrainieczarow");
+        Osoba innaOsoba2 = new Osoba("Dagmara","Drakula");
+
     }
+
+
 }
