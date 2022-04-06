6aed3a6f3fdf6acec1d1f7fd98dadcc5cc01e9ac
bKlamann92 <bartosz.klamann@gmail.com>
2022-04-06 15:17:54 +0200

    Dodana klasa i obiekt Bear
    Dodane Misie
    
    Zmiana3

--- src/main/java/com/sda/arppl2/Main.java
+++ src/main/java/com/sda/arppl2/Main.java
@@ -1,7 +1,29 @@
 package com.sda.arppl2;
 
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Zadanie 1 wykonane");
+
+
+        Bear bear1 = new Bear();
+        bear1.imie = "Marian";
+        bear1.wiek = 32;
+
+        Bear bear2 = new Bear();
+        bear2.imie = "Olek";
+        bear2.wiek = 2;
+
+        Bear bear3 = new Bear();
+        bear3.imie = "Filipek";
+        bear3.wiek = 10;
+
+        Bear bear4 = new Bear();
+        bear4.imie = "Teddy";
+        bear4.wiek = 3;
+
+
+
+
     }
 }
