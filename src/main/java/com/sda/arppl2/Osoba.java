8dc638f93dbe1463be3125a3bcccc266d3e6a81e
Jakub Pilarek <pilarek.jakub@gmail.com>
2022-04-06 19:42:25 +0200

    pierwsze

--- src/main/java/com/sda/arppl2/Osoba.java
+++ src/main/java/com/sda/arppl2/Osoba.java
@@ -1,2 +1,10 @@
-package com.sda.arppl2;public class Osoba {
+package com.sda.arppl2;
+
+public class Osoba {
+    String imie;
+    String nazwisko;
+    public Osoba(String imie, String nazwisko) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+    }
 }
