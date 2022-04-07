5e329798a317dc91b8a66a48f00c5d876b92fd99
Artur Ciepielowski <artciepi@gmail.com>
2022-04-07 23:28:37 +0200

    Zadanie 6 finnished

--- src/main/java/com/sda/arppl2/Main.java
+++ src/main/java/com/sda/arppl2/Main.java
@@ -1,15 +1,24 @@
+
 package com.sda.arppl2;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Zadanie 1 wykonane");
-
-        Osoba osoba = new Osoba("Marian","Kowalski");
-        Osoba innaOsoba = new Osoba("Adrian","Roztocki");
-        Osoba innaOsoba1 = new Osoba("Alicja", "Wkrainieczarow");
-        Osoba innaOsoba2 = new Osoba("Dagmara","Drakula");
+        Scanner scanner = new Scanner(System.in);
 
 
+        System.out.println("Jak masz na imie ");
+        String name = scanner.nextLine();
+
+        System.out.println("Jak się nazywasz? ");
+        String surname = scanner.nextLine();
+
+        Osoba osoba = new Osoba(name,surname);
+
+
+        System.out.println("Twoja osoba ma na imie: "+osoba.imie+ " a nazywa się : "+ osoba.nazwisko);
 
 
     }
